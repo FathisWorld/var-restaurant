a07d3fb12acc0cfc245daccb328a9e0f85364b8f
Mohamed Fathi <91429601+FathisWorld@users.noreply.github.com>
2023-02-07 18:41:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,392 @@
+// strat counter
+//end counter
+
+// addTcart.onclick = () => {
+//   counter.innerHTML +=1;
+//   counter.style.top = "0px";
+// }
+
+
+
+// ************************************************
+// Shopping Cart API
+// ************************************************
+
+var shoppingCart = (function() {
+  // =============================
+  // Private methods and propeties
+  // =============================
+  cart = [];
+  
+  // Constructor
+  function Item(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+  }
+  
+  // Save cart
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+  
+    // Load cart
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  }
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
+  }
+  
+
+  // =============================
+  // Public methods and propeties
+  // =============================
+  var obj = {};
+  
+  // Add to cart
+  obj.addItemToCart = function(name, price, count) {
+    for(var item in cart) {
+      if(cart[item].name === name) {
+        cart[item].count ++;
+        saveCart();
+        return;
+      }
+    }
+    var item = new Item(name, price, count);
+    cart.push(item);
+    saveCart();
+  }
+  // Set count from item
+  obj.setCountForItem = function(name, count) {
+    for(var i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count = count;
+        break;
+      }
+    }
+  };
+  // Remove item from cart
+  obj.removeItemFromCart = function(name) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart[item].count --;
+          if(cart[item].count === 0) {
+            cart.splice(item, 1);
+          }
+          break;
+        }
+    }
+    saveCart();
+  }
+
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function(name) {
+    for(var item in cart) {
+      if(cart[item].name === name) {
+        cart.splice(item, 1);
+        break;
+      }
+    }
+    saveCart();
+  }
+
+  // Clear cart
+  obj.clearCart = function() {
+    cart = [];
+    saveCart();
+  }
+
+  // Count cart 
+  obj.totalCount = function() {
+    var totalCount = 0;
+    for(var item in cart) {
+      totalCount += cart[item].count;
+    }
+    return totalCount;
+  }
+
+  // Total cart
+  obj.totalCart = function() {
+    var totalCart = 0;
+    for(var item in cart) {
+      totalCart += cart[item].price * cart[item].count;
+    }
+    return Number(totalCart.toFixed(2));
+  }
+
+  // List cart
+  obj.listCart = function() {
+    var cartCopy = [];
+    for(i in cart) {
+      item = cart[i];
+      itemCopy = {};
+      for(p in item) {
+        itemCopy[p] = item[p];
+
+      }
+      itemCopy.total = Number(item.price * item.count).toFixed(2);
+      cartCopy.push(itemCopy)
+    }
+    return cartCopy;
+  }
+
+  // cart : Array
+  // Item : Object/Class
+  // addItemToCart : Function
+  // removeItemFromCart : Function
+  // removeItemFromCartAll : Function
+  // clearCart : Function
+  // countCart : Function
+  // totalCart : Function
+  // listCart : Function
+  // saveCart : Function
+  // loadCart : Function
+  return obj;
+})();
+
+
+// *****************************************
+// Triggers / Events
+// ***************************************** 
+// Add item
+$('.add-to-cart').click(function(event) {
+  event.preventDefault();
+  var name = $(this).data('name');
+  var price = Number($(this).data('price'));
+  shoppingCart.addItemToCart(name, price, 1);
+  displayCart();
+  let counter = document.querySelector(".counter");
+  counter.style.top = "0px";
+});
+
+// Clear items
+$('.clear-cart').click(function() {
+  shoppingCart.clearCart();
+  displayCart();
+});
+
+
+function displayCart() {
+  var cartArray = shoppingCart.listCart();
+  var output = "";
+  for(var i in cartArray) {
+    output += "<tr>"
+      + "<td>" + cartArray[i].name + "</td>" 
+      + "<td>(" + cartArray[i].price + ")</td>"
+      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+      + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+      + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+      + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+      + " = " 
+      + "<td>" + cartArray[i].total + "</td>" 
+      +  "</tr>";
+  }
+  $('.show-cart').html(output);
+  $('.total-cart').html(shoppingCart.totalCart());
+  $('.total-count').html(shoppingCart.totalCount());
+}
+
+// Delete item button
+
+$('.show-cart').on("click", ".delete-item", function(event) {
+  var name = $(this).data('name')
+  shoppingCart.removeItemFromCartAll(name);
+  displayCart();
+})
+
+
+// -1
+$('.show-cart').on("click", ".minus-item", function(event) {
+  var name = $(this).data('name')
+  shoppingCart.removeItemFromCart(name);
+  displayCart();
+})
+// +1
+$('.show-cart').on("click", ".plus-item", function(event) {
+  var name = $(this).data('name')
+  shoppingCart.addItemToCart(name);
+  displayCart();
+})
+
+// Item count input
+$('.show-cart').on("change", ".item-count", function(event) {
+   var name = $(this).data('name');
+   var count = Number($(this).val());
+  shoppingCart.setCountForItem(name, count);
+  displayCart();
+});
+
+displayCart();
+
+
+
+//start cart & order
+let none  = document.querySelectorAll(".none");
+let order = document.querySelector(".order");
+let orderBtn = document.querySelector("#order");
+let home = document.querySelector("#home")
+let cartBtn = document.querySelector(".cart-btn")
+let cartcon = document.querySelector(".modal")
+let closeCart = document.querySelector(".close")
+let cartOrdar = document.querySelector(".cart-order")
+
+cartBtn.onclick = () => {
+  cartcon.style.transform = "translateY(0)"
+}
+
+closeCart.onclick = () => {
+  cartcon.style.transform = "translateY(-100%)"
+}
+
+orderBtn.addEventListener("click", () => {
+  order.style.display = "block";
+  none.forEach((el) => {
+    el.style.display = "none"
+  });
+});
+
+cartOrdar.addEventListener("click", () => {
+  cartcon.style.transform = "translateY(-100%)"
+  order.style.display = "block";
+  none.forEach((el) => {
+    el.style.display = "none"
+  });
+});
+
+home.onclick = () => {
+  order.style.display = "none";
+  none.forEach((el) => {
+    el.style.display = "block"
+  });
+}
+
+//end cart & order
+
+
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+let dishesBox = document.querySelector(".dishes .box-container");
+let dishe = document.querySelector(".dishes");
+let homeContent = document.querySelectorAll(".home .home-slider .slide .content");
+let aboutImg = document.querySelector(".about .row .image");
+let aboutContent = document.querySelector(".about .row .content");
+let about = document.querySelector(".about");
+let menuBox = document.querySelector(".menu .box-container");
+let menus = document.querySelector(".menu");
+let footerCredit = document.querySelector(".footer .credit");
+let footer = document.querySelector(".footer");
+
+window.onscroll = () =>{
+
+  if (window.scrollY >= dishe.offsetTop - 400) {
+    dishesBox.style.transform = "scale(1)";
+  }
+  if (window.scrollY >= about.offsetTop - 400) {
+    aboutImg.style.transform = "translateX(0)";
+    aboutContent.style.transform = "translateX(0)";
+    aboutImg.style.opacity = "1";
+    aboutContent.style.opacity = "1";
+  }
+  if (window.scrollY >= menus.offsetTop - 400) {
+    menuBox.style.transform = "scale(1)";
+  }
+  if (window.scrollY >= footer.offsetTop - 500) {
+    footerCredit.style.transform = "translateX(0)";
+  }
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// function loader(){
+//   let load = document.querySelector('.loader-container');
+//   load.style.display = "flex";
+// }
+
+// function fadeOut(){
+//   setInterval(loader, 3000);
+// }
+
+function textUp() {
+  homeContent.forEach((el) => {
+    el.style.cssText = "transform: scale(1);"
+    });
+}
+
+// window.onload = fadeOut;
+window.onload = textUp;
+
